4bd2fa37510f79e842eed6d1a1d4efdcfeea83d5
seojeong <seojeong1101@gmail.com>
2023-05-10 17:18:25 +0900

    타임리프 기본기능

---
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -0,0 +1,36 @@
+package hello.thymeleaf.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+
+    @GetMapping("/text-basic")
+    public String textBasic(Model model) {
+        model.addAttribute("data","Hello Spring!");
+        return "basic/text-basic";
+    }
+
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "basic/attribute";
+    }
+
+    @GetMapping("/each")
+    public String each(Model model) {
+//        model.addAttribute("")
+        return "basic/each";
+    }
+
+    @GetMapping("/condition")
+    public String condition(Model model) {
+//        addUsers(model);
+        return "basic/condition";
+    }
+
+
+}
